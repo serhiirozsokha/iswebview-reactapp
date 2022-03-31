67d44ed4b21e67c43fa6f6c5c02c6299d0e61e1b
serhii.rozsokha <serhii.rozsokha@pm.bet>
2022-03-31 15:58:13 +0300

    added isWebView lib

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import isWebview from 'is-ua-webview';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  debugger;
+  const webview = isWebview(navigator.userAgent);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {webview ? "isWebview" : "Browser"}
         </p>
         <a
           className="App-link"
